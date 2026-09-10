ff189dbd3dc98809e2da00672c19f4e7acfaaeca
kxnooooo <nidzasp08@gmail.com>
2026-09-10 19:20:16 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "waiting for cmds", // required by Discord API, not shown in the client
         state: "meow :3",     // this is what people actually see
         type: 2,               // Custom
       },
